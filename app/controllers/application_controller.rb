a78a56a83b366f7e5f9272836ef3cf91bc6a76ad
Nao199 <eddionk13@gmail.com>
2024-09-10 21:45:47 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :check_user_authentication, except: [:new, :create]
 
@@ -14,6 +15,12 @@ class ApplicationController < ActionController::Base
   def check_user_authentication
     unless user_signed_in?
       redirect_to splashes_path
+    end
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
 
