b20b94fea7ca5a029cf20af42713451f39818553
Nao199 <eddionk13@gmail.com>
2024-09-23 21:55:11 +0900

    ルーティングのコメント追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,27 @@
 Rails.application.routes.draw do
+  # ユーザー認証のためのDeviseルーティング
   devise_for :users
-  root to:'foods#index'
 
+  # アプリケーションのルートパスを設定
+  root to: 'foods#index'
+
+  # 食品関連のリソースルーティング
   resources :foods do
     member do
+      # 個別の食品に対する予約アクション
       post 'reserve'
+      # 取引完了アクション
       post 'complete_transaction'
     end    
     collection do
+      # 食品検索アクション
       get 'search'
     end
   end
 
-  resources :splashes , only: [:index]
-  resources :users, only: [:show]
+  # スプラッシュ画面用のルーティング
+  resources :splashes, only: [:index]
 
+  # ユーザープロフィール表示用のルーティング
+  resources :users, only: [:show]
 end
